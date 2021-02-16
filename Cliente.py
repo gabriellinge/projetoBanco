a9822ddccab55166263d1bcce7afad4f15b1b05f
Gabriel Linge <liinge@hotmail.com>
2021-02-15 21:39:26 -0300

    Atualização para validar CPF e Nome

--- Cliente.py
+++ Cliente.py
@@ -4,9 +4,21 @@ class Cliente:
    def __init__(self, clienteCPF, clienteNome):
       """
       Iniciar o cadastro do cliente
-      :clienteCPF = CPF com 9 caracteres
+      :clienteCPF = CPF com 11 números
       :clienteNome = nome completo do cliente
       """
-      
-      self.clienteCPF = clienteCPF
-      self.clienteNome = clienteNome
+      self.cpf = ''
+      self.nomeCompleto = ' '.join(clienteNome.rsplit()).title()
+      self.validarCPF(clienteCPF)
+
+   def validarCPF(self, clienteCPF):
+      if len(str(clienteCPF)) == 11:
+         cpf = [
+                [i for i in clienteCPF[0:3] ],
+                [i for i in clienteCPF[3:6] ],
+                [i for i in clienteCPF[6:9] ],
+                [i for i in clienteCPF[9:12]]
+               ]
+         for i in cpf:
+            self.cpf += ''.join(i) + '-'
+         self.cpf = self.cpf.rstrip('-')
